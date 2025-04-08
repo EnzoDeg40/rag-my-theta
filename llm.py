0fcfae7649e12908e8830f828697b2844eec9990
Enzo Degraeve <enzodeg40@gmail.com>
2025-04-08 09:36:29 +0200

    Convert script into class

--- llm.py
+++ llm.py
@@ -1,8 +1,20 @@
 from litellm import completion
 
-response = completion(
-    model="ollama/mistral", 
-    messages=[{ "content": "respond in 20 words. who are you?","role": "user"}], 
-    api_base="http://localhost:11434"
-)
-print(response)
+class LLM:
+    def __init__(self, model: str = "ollama/mistral", api_base: str = "http://localhost:11434"):
+        self.model = model
+        self.api_base = api_base
+
+    def ask(self, text: str, use_vector_db: bool = True):
+        response = completion(
+            model=self.model,
+            messages=[{"content": text, "role": "user"}],
+            api_base=self.api_base
+        )
+        return response
+
+if __name__ == "__main__":
+    llm = LLM()
+    text = "What is the purpose of the document?"
+    response = llm.ask(text)
+    print(f"LLM Response: {response}")
